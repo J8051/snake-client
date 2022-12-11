1da9656f19f6d066ced8414f766b209809e90b50
J8051 <nathaniel.jessica@yahoo.ca>
2022-12-11 17:20:24 -0500

    the connect portion of the game was moved to its own file

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require("net"); 
-//establishes a connection with the game server
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localHost",
-    port: 50541,
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => { 
-    console.log("server says:", data); 
-  })
-  
-  return conn;
-}; 
+const {connect}  = require("./client"); 
 
 console.log("connecting..."); 
 connect(); 
