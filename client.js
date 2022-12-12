e9053d57ec9794aaea57fc28d4a117b0e941abbc
J8051 <nathaniel.jessica@yahoo.ca>
2022-12-11 19:42:21 -0500

    all the connection related functionality has been moved into this file.

--- client.js
+++ client.js
@@ -1,14 +1,22 @@
+const { Server } = require("http");
 const net = require("net"); 
+
 const connect = function () {
   const conn = net.createConnection({
     host: "localHost",
     port: 50541,
   });
-  // interpret incoming data as text
+
   conn.setEncoding("utf8");
-  conn.on("data", (data) => { 
-    console.log("server says:", data); 
-  })
+  //listens to data input given to terminal
+  conn.on("data", (data) => {
+    console.log("server says:", data);
+  });
+// when connection established it will run the fucntion inside it 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server")
+    conn.write('Name: JEN')
+  }); 
   
   return conn;
 }; 
